2d1951efd0db9c5ddfef08604c98458ab60b3278
BaskaKlim <baska.klimekova@gmail.com>
2020-12-05 21:32:35 +0100

    prime number check method

---
+++ prvocislo_demo/prvocislo_logic/__init__.py
@@ -0,0 +1,23 @@
+"""
+prvocislo.py
+Vytvoř program, který vyhodnotí,
+zdali je vložené číslo prvočíslo.
+Prvočísla mají právě dva dělitele,
+jsou dělitelná pouze 1 a sami sebou.
+"""
+
+cislo = input("Vlož číslo: ")
+
+if cislo.isdigit() and int(cislo) > 0:
+    cislo = int(cislo)
+    delitele = []
+    for i in range(1, cislo + 1):
+        if cislo % i == 0:
+            delitele.append(i)
+    print(f"Dělitelé: {delitele}")
+    if len(delitele) == 2:
+        print(f"Číslo {cislo} je prvočíslo.")
+    else:
+        print(f"Číslo {cislo} není prvočíslo.")
+else:
+    print("Nebylo zadáno kladné číslo.")
